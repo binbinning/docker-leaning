a8b7169de7a1c25ddcc6ea5686f155ddeaeb7104
nick <binbinnjtu@163.com>
2018-02-25 22:58:45 -0800

    update shell

--- docker_rm_all.sh
+++ docker_rm_all.sh
@@ -4,5 +4,7 @@
 # change history
 #   17.11.27	nick	initial version
 #
-docker ps -a > tmp
-cat tmp|cut -b 1-12|grep -v CON| while read LINE; do docker stop $LINE; docker rm $LINE; done
+#docker ps -a > tmp
+#cat tmp|cut -b 1-12|grep -v CON| while read LINE; do docker stop $LINE; docker rm $LINE; done
+
+docker container rm $(docker container ls -a -q)         # Remove all containers
